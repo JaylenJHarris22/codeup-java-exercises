690431d0f0a5a189f1d331cc0307d96bf1466084
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-04-10 10:05:35 -0500

    finished arrays assignment

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -6,21 +6,46 @@ import java.util.Arrays;
 public class MoviesApplication {
     public static void main(String[] args) {
         Input input = new Input();
+        int userNum;
 
-//        System.out.println("1 - to view all movies\n2 - to choose animated movies.\n" +
-//                "3 - to view drama movies\n4 - to view horror movies\n5 - to view sci-fi movies");
-//        int userNum = input.getInt(1, 5);
-//
-//        if (userNum == 1 ){
-//            System.out.println(Arrays.toString(MoviesArray.findAll()));
-//        } else if (userNum == 2) {
-//            System.out.println(Arrays.toString(MoviesArray.findAll()));
-//        }
-        Movie[] movies = (MoviesArray.findAll());
-
-        System.out.println((movies[45]));
-
-        System.out.println(Movie.get);
-
+        do {System.out.println("0 - exit\n1 - to view all movies\n2 - to choose animated movies.\n" +
+                "3 - to view drama movies\n4 - to view horror movies\n5 - to view sci-fi movies\n6 - add movie");
+            userNum = input.getInt(0, 6);
+            if (userNum == 1 ){
+                for( Movie movie : MoviesArray.findAll()){
+                    System.out.println(movie + "\n\n");
+                }
+            } else if (userNum == 2) {
+                for( Movie movie : MoviesArray.findAll()){
+                    if (movie.getCategory().equals("animated")){
+                        System.out.println(movie + "\n\n");
+                    }
+                }
+            } else if (userNum == 3) {
+                for( Movie movie : MoviesArray.findAll()){
+                    if (movie.getCategory().equals("drama")){
+                        System.out.println(movie + "\n\n");
+                    }
+                }
+            } else if (userNum == 4) {
+                for( Movie movie : MoviesArray.findAll()){
+                    if (movie.getCategory().equals("horror")){
+                        System.out.println(movie + "\n\n");
+                    }
+                }
+            } else if (userNum == 5) {
+                for( Movie movie : MoviesArray.findAll()){
+                    if (movie.getCategory().equals("scifi")){
+                        System.out.println(movie + "\n\n");
+                    }
+                }
+            } else if (userNum == 6) {
+                for( Movie movie : MoviesArray.findAll()){
+                    if (movie.getCategory().equals("scifi")){
+                        System.out.println(movie + "\n\n");
+                    }
+                }
+            }
+        } while (userNum !=6);
     }
 }

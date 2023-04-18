177b53b2fd5922a0377629ac809631be229fe169
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-04-18 11:21:15 -0500

    finished the exceptions and error handling exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -18,33 +18,107 @@ public class Input {
         }
         return false;
     }
+//    public int getInt(int min, int max){
+//        int input;
+//        do{
+//            System.out.println("Enter a number that is within " + min + " and " + max + ".");
+//            input = Integer.parseInt(scanner.nextLine());
+//        } while (input < min || input > max);
+//
+//        return input;
+//    }
+//    public int getInt(){
+//        System.out.println("Give me a number.");
+//        int input = Integer.parseInt(scanner.nextLine());
+//        return input;
+//    }
+
+    public int getInt(){
+        int number = 0;
+        try{
+            System.out.println("Give me a number.");
+            number = Integer.valueOf(getString());
+        } catch (NumberFormatException e){
+            System.out.println("That was not a number");
+        }
+        return number;
+    }
+
     public int getInt(int min, int max){
-        int input;
+        int number = 0;
         do{
             System.out.println("Enter a number that is within " + min + " and " + max + ".");
-            input = Integer.parseInt(scanner.nextLine());
-        } while (input < min || input > max);
+            try{
+                System.out.println("Give me a number.");
+                number = Integer.valueOf(getString());
+            } catch (NumberFormatException e){
+                System.out.println("That was not a number");
+            }
+        } while (number < min || number > max);
 
-        return input;
+        return number;
     }
-    public int getInt(){
-        System.out.println("Give me a number.");
-        int input = Integer.parseInt(scanner.nextLine());
-        return input;
-    }
+
+//    double getDouble(double min, double max){
+//        double input;
+//        do{
+//            System.out.println("Enter a number that is within my range.");
+//            input = Double.parseDouble(scanner.nextLine());
+//        } while (input < min || input > max);
+//
+//        return input;
+//    }
+//    public double getDouble(){
+//        System.out.println("Give me a decimal number.");
+//        double input = Double.parseDouble(scanner.nextLine());
+//        return input;
+//    }
+
     double getDouble(double min, double max){
-        double input;
+        double number = 0;
         do{
             System.out.println("Enter a number that is within my range.");
-            input = Double.parseDouble(scanner.nextLine());
-        } while (input < min || input > max);
+            try{
+                System.out.println("Give me a number.");
+                number = Double.valueOf(getString());
+            } catch (NumberFormatException e){
+                System.out.println("That was not a number");
+            }
+        } while (number < min || number > max);
 
-        return input;
+        return number;
     }
     public double getDouble(){
+        double number = 0;
         System.out.println("Give me a decimal number.");
-        double input = Double.parseDouble(scanner.nextLine());
-        return input;
+        try{
+            System.out.println("Give me a number.");
+            number = Double.valueOf(getString());
+        } catch (NumberFormatException e){
+            System.out.println("That was not a number");
+        }
+        return number;
     }
 
+    public int getBinary(){
+        int number = 0;
+        try{
+            System.out.println("Give me a number.");
+            number = Integer.valueOf(getString(), 2);
+        } catch (NumberFormatException e){
+            System.out.println("That was not a number");
+        }
+        return number;
+    }
+
+    public int getHex(){
+        int number = 0;
+        try{
+            System.out.println("Give me a number.");
+            number = Integer.valueOf(getString(), 16);
+        } catch (NumberFormatException e){
+            System.out.println("That was not a number");
+        }
+        return number;
+    }
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,11 +4,13 @@ public class InputTest {
     public static void main(String[] args) {
         Input input = new Input();
 
-        System.out.println(input.getString());;
-        System.out.println(input.yesNo());
-        System.out.println(input.getInt(1,7));
-        System.out.println(input.getInt());
-        System.out.println(input.getDouble(25, 79));
-        System.out.println(input.getDouble());
+//        System.out.println(input.getString());;
+//        System.out.println(input.yesNo());
+//        System.out.println(input.getInt(1,7));
+//        System.out.println(input.getInt());
+//        System.out.println(input.getDouble(25, 79));
+//        System.out.println(input.getDouble());
+        System.out.println(input.getHex());
+        System.out.println(input.getBinary());
     }
 }

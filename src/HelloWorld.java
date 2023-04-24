1faad722911e2bb2f13c434bd461076810a8b3d1
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-04-24 08:57:34 -0500

    helloworld push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,47 @@
+import javax.sound.midi.SoundbankResource;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World.");
+
+    int myFavoriteNumber = 7;
+    System.out.println(myFavoriteNumber);
+       String myString = "Minecraft";
+    //    String myString = 'j';
+    //    String myString = 3.14159;
+    // System.out.println(myString);
+    // long myNumber;
+    // myNumber = 3.14;
+    // long myNumber = 123;
+    // System.out.println(myNumber);
+    // float myNumber = 3.14f;
+    //    double bigNumber = -2083717234;
+
+    //    int x = 5;
+    //    System.out.println(x++);
+    //    System.out.println(x);
+
+    //    int x = 5;
+    //    System.out.println(++x);
+    //    System.out.println(x);
+
+    // String class = "hey";
+    //     String theNumberThree = "three";
+    // Object o = theNumberThree;
+    // int three = (int) o;
+    // int three = (int) "three";
+
+    // int x = 4;
+    // x += 5;
+
+    // int x = 3;
+    // int y = 4;
+    // y *= x;
+
+    // int x = 10;
+    // int y = 2;
+    // x /= y;
+    // y -= x;
+
     }
 }
